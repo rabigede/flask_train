60230882570f045b599e4fdc48aa3dd58349f67a
katya motos <kotosmata@gmail.com>
2025-06-14 22:44:59 +0300

    fix: now it works

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template
+from app import app
 
+@app.route('/index')
 @app.route('/')
 def index():
-    return render_template('index.html')
+    username = "matya kotos"
+    return render_template('index.html',
+                           username = username)
 
 @app.route('/albums/<int:album_number>')
 def albums(album_number):
