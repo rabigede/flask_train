dc79b967bfc3ab9e146a30bad651486925fafb80
katya motos <kotosmata@gmail.com>
2025-06-15 00:13:59 +0300

    add: create base template

--- app/routes.py
+++ app/routes.py
@@ -4,9 +4,16 @@ from app import app
 @app.route('/index')
 @app.route('/')
 def index():
-    username = "matya kotos"
+    username = "Екатерина Мотос"
+    news=[
+        "Дельфины перестали удовлетворять своих самок. Теперь они перешли на ваших детей...",
+        "Павел Техник скончался сегодня в своей квартире в Дубае",
+        "СТРАНА УЖАСНУЛАСЬ, УВИДЕВ ЧТО СТАЛО С АЛИШЕРОМ МОРГЕНШТЕРНОМ ПОСЛЕ ГОДА В РЕХАБЕ",
+        "УЧЁНЫЕ ТВЕРДЯТ: СТАЛИН НЕ ЕЛ ДЕТЕЙ НА ЗАВТРАК! ОН ЕЛ ИХ НА УЖИН..."
+    ]
     return render_template('index.html',
-                           username = username)
+                           username=username,
+                           news=news)
 
 @app.route('/albums/<int:album_number>')
 def albums(album_number):
